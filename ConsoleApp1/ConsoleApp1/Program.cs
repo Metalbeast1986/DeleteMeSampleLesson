58681fdfe847791b836a0bae2df9a8f58412ce2e
ksistof1986 <ksistof1986@gmail.com>
2019-12-12 20:40:45 +0200

    Third function test

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(isPrimal(12));
+            Console.WriteLine(isPrimal(1));
+        }
+        static bool isPrimal(int num)
+        {
+            bool result = true;
+            for (int i=2; i < num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    result = false;
+                    break;
+                }
+            } 
+            return result;
         }
     }
 }
